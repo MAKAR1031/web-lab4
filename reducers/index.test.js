eb83f3d5f35938bb3b6c5a0b5fc2fb7a558f4a03
Sergey Makarov <makar1031@yandex.ru>
2017-12-08 22:53:00 +0300

    добавлены тесты для действий

--- reducers/index.test.js
+++ reducers/index.test.js
@@ -1,12 +1,74 @@
 import reducers from './index';
 
 describe('todos', () => {
-  test('returns [] by default', () => {
-    const reducer = reducers.todos;
+    test('returns [] by default', () => {
+        const reducer = reducers.todos;
 
-    const action = { type: null };
-    const newState = reducer(null, action);
+        const action = {
+            type: null
+        };
+        const newState = reducer(null, action);
 
-    expect(newState).toEqual([]);
-  });
+        expect(newState).toEqual([]);
+    });
+});
+
+describe('todos', () => {
+    test('add todo', () => {
+        const reducer = reducers.todos;
+
+        const todoText = "Test todo"
+        const action = {
+            type: 'ADD_TODO',
+            text: todoText
+        };
+        const newState = reducer([], action);
+
+        expect(newState).toEqual([{
+            id: 1,
+            text: todoText,
+            priority: false
+        }]);
+    });
+});
+
+describe('todos', () => {
+    test('change todo priority', () => {
+        const reducer = reducers.todos;
+
+        const action = {
+            type: 'CHANGE_PRIORITY',
+            id: 1
+        };
+        const newState = reducer([{
+            id: 1,
+            text: "Test todo",
+            priority: false
+        }], action);
+
+        expect(newState[0].priority).toBeTruthy()
+    });
+});
+
+describe('todos', () => {
+    test('remove all todos', () => {
+        const reducer = reducers.todos;
+
+        const action = {
+            type: 'REMOVE_ALL'
+        };
+        const newState = reducer([{
+                id: 1,
+                text: "Test todo 1",
+                priority: false,
+            },
+            {
+                id: 2,
+                text: "Test todo 2",
+                priority: false,
+            }
+        ], action);
+
+        expect(newState).toEqual([]);
+    });
 });
